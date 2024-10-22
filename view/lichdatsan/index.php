12edd2cbdb7fdbc9e4c4178ff53739d6e8575d17
KoaLy <lyanhkhoa789@gmail.com>
2024-10-22 23:30:13 +0700

    khong doi

--- view/lichdatsan/index.php
+++ view/lichdatsan/index.php
@@ -421,10 +421,10 @@ if($tbl===-1){
         </table>
     </div>
 </form>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+    
 </body>
 </html>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 <script>
     $(document).ready(function () {
         var tongtien = 0;
